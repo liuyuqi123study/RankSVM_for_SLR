b2d51c1d36bb64a39e9a3354298c1df039e09cc6
Yuqi Liu <123962595+liuyuqi123study@users.noreply.github.com>
2025-03-05 10:27:43 +0800

    hiii

--- CAIL2024/LEVEN-main-2/Downstreams/SCR/SCR-Experiment_me/feature_extraction_lawformer.sh
+++
@@ -1,2 +0,0 @@
-python feature_extraction_lawformer.py --config ./config/pairwise/lawformer.config --gpu 0 --do_test --test_file /content/drive/MyDrive/CAIL2024/LEVEN-main-2/Downstreams/SCR/SCR-Preprocess/output_data-supervised/query/test_query.json --checkpoint /content/input/model/1.pkl
-
