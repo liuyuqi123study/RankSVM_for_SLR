caa0c61d7434a99a50662163e57be674740b6082
Yuqi Liu <123962595+liuyuqi123study@users.noreply.github.com>
2025-03-09 10:47:07 +0800

    hi

--- svm_rank/svm_rank_mamba.sh
+++ svm_rank/svm_rank_mamba.sh
@@ -1,9 +1,9 @@
-svm_rank_learn train_feature0.dat model_0_mamba.dat
-svm_rank_learn train_feature1.dat model_1_mamba.dat
-svm_rank_learn train_feature2.dat model_2_mamba.dat
-svm_rank_learn train_feature3.dat model_3_mamba.dat
-svm_rank_learn train_feature4.dat model_4_mamba.dat
-svm_rank_classify test_feature0.dat model_0_mamba.dat
+svm_rank_learn RankMamba/Results/train_feature0.dat RankMamba/Results/model_0_mamba.dat
+svm_rank_learn RankMamba/Results/train_feature1.dat RankMamba/Results/model_1_mamba.dat
+svm_rank_learn RankMamba/Results/train_feature2.dat RankMamba/Results/model_2_mamba.dat
+svm_rank_learn RankMamba/Results/train_feature3.dat RankMamba/Results/model_3_mamba.dat
+svm_rank_learn RankMamba/Results/train_feature4.dat RankMamba/Results/model_4_mamba.dat
+svm_rank_classify RankMamba/Results/test_feature0.dat model_0_mamba.dat
 svm_rank_classify test_feature1.dat model_1_mamba.dat
 svm_rank_classify test_feature2.dat model_2_mamba.dat
 svm_rank_classify test_feature3.dat model_3_mamba.dat
