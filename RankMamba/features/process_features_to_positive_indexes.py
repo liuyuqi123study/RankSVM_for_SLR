2c06c9e111927ac830b4d8abb643ba0f64a5b02c
Yuqi Liu <123962595+liuyuqi123study@users.noreply.github.com>
2025-03-10 10:40:52 +0800

    hi

---
+++ RankMamba/features/process_features_to_positive_indexes.py
@@ -0,0 +1,27 @@
+# Reading text data from a .dat file
+#This is the second step to deal with positive indexes of those features
+for i in range(5):
+
+    #We first modify the training features
+    with open('train_feature'+str(i)+'.dat', 'r') as file:
+        rows = file.readlines()
+        modified_rows=[]
+        for row in rows:
+            modified_row=row.strip('"')
+            #We write it into some specific format
+            modified_row=modified_row.split(' ',maxsplit=1)[0][-2]+' '+modified_row.split(' ',maxsplit=1)[1]
+            modified_rows.append(modified_row)
+    with open('train_feature'+str(i)+'.dat','w') as file:
+        file.writelines(modified_rows)
+
+    #Then we modify test data
+    with open('test_feature'+str(i)+'.dat', 'r') as file:
+        rows = file.readlines()
+        modified_rows=[]
+        for row in rows:
+            modified_row=row.strip('"')
+            #We write it into some specific format
+            modified_row=modified_row.split(' ',maxsplit=1)[0][-2]+' '+modified_row.split(' ',maxsplit=1)[1]
+            modified_rows.append(modified_row)
+    with open('test_feature'+str(i)+'.dat','w') as file:
+        file.writelines(modified_rows)
