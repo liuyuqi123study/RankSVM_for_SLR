e6074bc155d5a7a3f071d7b22b89b4037facee3c
Yuqi Liu <123962595+liuyuqi123study@users.noreply.github.com>
2025-03-11 09:36:13 +0800

    heyy

--- svm_rank/svm_rank_mamba.sh
+++ svm_rank/svm_rank_mamba.sh
@@ -1,10 +1,13 @@
-./svm_rank_learn -c 0.01 /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified0.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_0_mamba.dat
-./svm_rank_learn -c 0.01 /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified1.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_1_mamba.dat
-./svm_rank_learn -c 0.01 /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified2.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_2_mamba.dat
-./svm_rank_learn -c 0.01 /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified3.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_3_mamba.dat
-./svm_rank_learn -c 0.01 /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified4.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_4_mamba.dat
-./svm_rank_classify /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified0.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_0_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_0.dat
-./svm_rank_classify /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified1.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_1_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_1.dat
-./svm_rank_classify /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified2.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_2_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_2.dat
-./svm_rank_classify /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified3.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_3_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_3.dat
-./svm_rank_classify /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified4.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_4_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_4.dat
+for i in 0.005, 0.02, 0.05,0.1, 0.5, 1
+do
+    echo "./svm_rank_learn -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified0.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_0_mamba_{$i}.dat"
+    echo "./svm_rank_learn -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified1.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_1_mamba_{$i}.dat"
+    echo "./svm_rank_learn -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified2.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_2_mamba_{$i}.dat"
+    echo "./svm_rank_learn -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified3.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_3_mamba_{$i}.dat"
+    echo "./svm_rank_learn -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/train_feature_modified4.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_4_mamba_{$i}.dat"
+    echo "./svm_rank_classify -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified0.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_0_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_0_{$i}.dat"
+    echo "./svm_rank_classify -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified1.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_1_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_1_{$i}.dat"
+    echo "./svm_rank_classify -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified2.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_2_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_2_{$i}.dat"
+    echo "./svm_rank_classify -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified3.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_3_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_3_{$i}.dat"
+    echo "./svm_rank_classify -c $i /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/test_feature_modified4.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/model_4_mamba.dat /Users/yuqi/Downloads/RankSVM_for_SLR/RankMamba/features/predictions_4_{$i}.dat"
+done
